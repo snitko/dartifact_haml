b043bc9acdbbf7236bc1b016c08f87cc175d23da
Roman Snitko <roman.snitko@gmail.com>
2017-07-23 21:45:04 -0700

    Parsing data-component- attributes (name, property, attribute-properties)

--- lib/dartifact_haml/parser.rb
+++ lib/dartifact_haml/parser.rb
@@ -1,11 +1,94 @@
 module DartifactHaml
   class Parser < ::Haml::Parser
 
-    def process_line(line)
-      component_part = line.text.match(/\A%%.*?%/)[0]
-      line.text = line.text.sub(/\A%%.*?%/, "%")
-      super(line)
+    def process_line(haml_line)
+      component_part_string = haml_line.text.match(/\A%%.*?%/)[0]
+      haml_line.text        = haml_line.text.sub(/\A%%.*?%/, "%")
+
+      if component_part_string
+        haml_line.text = add_component_data_to_tag(component_part_parsed(component_part_string), haml_line.text)
+      end
+
+      super(haml_line)
     end
+
+    # Parses component_part string, creating a hash with
+    # each Component logical element content and a method to be called
+    # to insert this content into the tag.
+    #
+    # Input.:  component_part_string = "form_field(country_selector,selector)"
+    # Result: { name: "FormField", roles: "country_selector,selector" }
+    def component_part_parsed(string)
+      matches = string.chop.match(/%%([a-zA-Z0-9_]+)?\s*(.+)?\s*(.+)?\s*(\{.+\})?/)
+      parsed = {
+        name:      matches[1].split('_').collect(&:capitalize).join + "Component",
+        roles:     (matches[2].gsub(/[\(\)]/, "") if matches[2]),
+        property:  (matches[3].gsub(/[\(\)]/, "") if matches[3]),
+        attribute_properties: (matches[4].gsub(/[\{\}]/, "") if matches[4])
+      }
+      parsed.delete_if { |k,v| v.nil? }
+      parsed
+    end
+
+    # Modifies the original haml tag adding necessary data- attributes to indicate
+    # this is a component.
+    #
+    # Input: component_part_parsed = "form_field(country_selector,selector)"
+    #        haml_line             = "%input(name="country")
+    #
+    # Result: "%input(name="country"
+    #                 data-component-name="FormFieldComponent"
+    #                 data-component-roles="country_selector,selector")
+    def add_component_data_to_tag(component_part_parsed, haml_text)
+
+      # Parsing haml so we can later reconstruct it and insert our own
+      # attributes.
+      matches     = haml_text.match(/%([a-zA-Z0-9_]+)?\s*(\(.+\))?\s*(\{.+\})?(.*)/)
+      tag         = matches[1] || "div"
+      attrs_plain = matches[2] ? matches[2].gsub(/[\(\)]/, "") : ""
+      attrs_curly = matches[3] ? matches[3].gsub(/[\{\}]/, "") : ""
+      remainder   = matches[4]
+
+      component_part_parsed.each do |attr_name,value|
+        
+        # Turns attribute_properties passed into appropriate data- attributes.
+        # Example:
+        #   Passed:                 { attr_1: "value1", attr_2: "value2" }
+        #
+        #   Result (inside a tag) : data-component-property-attributes="attr_1:data-attr-1,attr_2:data-attr-2"
+        #                           data-attr-1="value1"
+        #                           data-attr-2="value2"
+        if attr_name == :attribute_properties
+          attribute_properties = []
+          parse_attribute_properties_hash(value).each do |k,v|
+            attrs_plain += " data-#{k}=\"#{v}\"" if v
+            attribute_properties << "#{k}:data-#{k.gsub("_", "-")}"
+          end
+          attrs_plain += " data-component-#{attr_name.to_s.gsub("_", "-")}=\"#{attribute_properties.join(",")}\""
+
+        # Take care of the rest of component data- attributes, such as
+        # data-component-name, data-component-roles and data-component-property
+        else
+
+          attrs_plain += " data-component-#{attr_name.to_s.gsub("_", "-")}=\"#{value}\""
+        end
+      end
+
+      "%#{tag}(#{attrs_plain}){#{attrs_curly}}#{remainder}"
+    end
+
+    # Attribute properties are passed as in a format that mirrors Ruby Hash. Example:
+    # { attribute1: "value1", attribute2: "value2" }.
+    # This method parses the string and turns it into an actual Ruby Hash.
+    def parse_attribute_properties_hash(s)
+      hash = {}
+      s.split(",").map { |i| i.lstrip!; i.rstrip }.each do |i|
+        k,v= i.split(/\s*:\s*/)
+        hash[k] = v.gsub(/"(.*)"/, '\1')
+      end
+      hash
+    end
+
 
   end
 end
--- spec/lib/dartifact_haml/haml_parser_spec.rb
+++ spec/lib/dartifact_haml/haml_parser_spec.rb
@@ -6,8 +6,33 @@ describe DartifactHaml::Parser do
     Haml::Engine.new(text, parser_class: DartifactHaml::Parser).to_html.rstrip
   end
 
-  it "separates component part from the tag part" do
-    expect(render("%%component_name%div hello")).to eq("<div>hello</div>")
+  describe "parser methods" do
+
+    before(:each) do
+      @parser = DartifactHaml::Parser.new([])
+    end
+
+
+    it "parses the component part of the haml line" do
+      result = @parser.component_part_parsed("%%form_field(country_selector,selector)")
+      expect(result).to eq({ name: "FormFieldComponent", roles: "country_selector,selector" })
+    end
+
+    it "inserts component-related attributes into the original haml" do
+      result = @parser.add_component_data_to_tag({ name: "FormFieldComponent", roles: "country_selector,selector" }, '%div(align="left")')
+      expect(result).to eq('%div(align="left" data-component-name="FormFieldComponent" data-component-roles="country_selector,selector"){}')
+    end
+
+    it "inserts component attribute properties and their values" do
+      result = @parser.add_component_data_to_tag({ name: "FormFieldComponent", attribute_properties: 'hello: "world"'}, '%div(align="left")')
+      expect(result).to eq('%div(align="left" data-component-name="FormFieldComponent" data-hello="world" data-component-attribute-properties="hello:data-hello"){}')
+    end
+
+
+  end
+
+  it "adds component name as a data attribute to the tag" do
+    expect(render("%%button%div hello")).to eq("<div data-component-name='ButtonComponent'>hello</div>")
   end
 
 end
